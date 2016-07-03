a99aff968c10b8da1c312b27f49e55cc8cf3117f
gary600 <gary600playsmc@gmail.com>
2016-07-03 10:29:27 -0600

    Continued work on server and added other stuff
    Continued working on the request handler in the Server, and added some
    example requests.

--- server/pydcrawl-server.py
+++ server/pydcrawl-server.py
@@ -2,13 +2,33 @@ import socket
 import socketserver
 import json
 import time
+import sys
+
+def log(str):
+	output = "[{}]: {}".format(time.strftime("%H:%M:%S"), str)
+	sys.stdout.write(output)
+	logfile = open("server.log", "a")
+	logfile.write(output)
+	logfile.close()
+
+class ConnectError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
 
 class ConnectionHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		self.dataRaw = self.request.recv(1024).strip()
 		try:
 			self.data = json.loads(self.dataRaw.decode())
-			print("[{}]: Connection from {} with username {}".format(time.strftime("%H:%M:%S"), self.client_address[0], self.data["user"]["name"]))
+			if self.data["connectType"] != "request_connect":
+				raise ConnectError
+			log("New connection from {} with username {}".format(self.client_address[0], self.data["data"]["username"]))
+			while True:
+				self.dataRaw = self.request.recv
 		except ValueError:
-			print("[{}]: Invalid JSON from {}".format(time.strftime("%H:%M:%S"), self.client_address[0]))
+			log("Invalid JSON from {}".format(self.client_address[0]))
+		except ConnectError:
+			log("Invalid request from {}".format(self.client_address[0]))
 		

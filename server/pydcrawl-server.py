0fb9a991af8e32035e73b9bec659cc695a651645
gary600 <gary600playsmc@gmail.com>
2016-07-03 11:59:01 -0600

    Added MOAR

--- server/pydcrawl-server.py
+++ server/pydcrawl-server.py
@@ -4,14 +4,25 @@ import json
 import time
 import sys
 
-def log(str):
-	output = "[{}]: {}".format(time.strftime("%H:%M:%S"), str)
+class loglevels:
+	INFO = "INFO"
+	WARN = "WARN"
+	FATAL = "FATAL"
+
+def log(str, loglevel):
+	output = "[{}/{}]: {}".format(time.strftime("%H:%M:%S"), loglevel, str)
 	sys.stdout.write(output)
 	logfile = open("server.log", "a")
 	logfile.write(output)
 	logfile.close()
 
 class ConnectError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+		
+class OutdatedClientError(Exception):
 	def __init__(self, value):
 		self.value = value
 	def __str__(self):
@@ -24,11 +35,13 @@ class ConnectionHandler(socketserver.BaseRequestHandler):
 			self.data = json.loads(self.dataRaw.decode())
 			if self.data["connectType"] != "request_connect":
 				raise ConnectError
-			log("New connection from {} with username {}".format(self.client_address[0], self.data["data"]["username"]))
+			if self.data["data"]["clientVersion"]
+			log("New connection from {} with username {}".format(self.client_address[0], self.data["data"]["username"]), loglevels.INFO)
 			while True:
-				self.dataRaw = self.request.recv
+				self.dataRaw = self.request.recv(1024).strip()
 		except ValueError:
-			log("Invalid JSON from {}".format(self.client_address[0]))
+			log("Invalid JSON from {}".format(self.client_address[0]), loglevels.INFO)
 		except ConnectError:
-			log("Invalid request from {}".format(self.client_address[0]))
-		
+			log("Invalid request from {}".format(self.client_address[0]), loglevels.WARN)
+		except OutdatedClientError:
+			log("Outdated client from {}".format(self.client_address[0]), loglevels.INFO)
